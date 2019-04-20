ec0bbf530c30e7ec83002381cc5a5fab8789bac6
Armin Hohenegger <armin695@gmail.com>
2019-04-20 13:31:00 +0200

    move german keys from tab-dance to a layer

--- keyboards/planck/keymaps/vaskosdune/keymap.c
+++ keyboards/planck/keymaps/vaskosdune/keymap.c
@@ -42,21 +42,6 @@ enum planck_keycodes {
   ZELDA
 };
 
-enum {
-  TD_O_OE = 0,
-  TD_A_AE = 1,
-  TD_U_UE = 2,
-  TD_S_SS = 3
-};
-
-qk_tap_dance_action_t tap_dance_actions[] = {
-  [TD_O_OE]  = ACTION_TAP_DANCE_DOUBLE(KC_O, GKC_OE),
-  [TD_A_AE] = ACTION_TAP_DANCE_DOUBLE(KC_A, GKC_AE),
-  [TD_U_UE] = ACTION_TAP_DANCE_DOUBLE(KC_U, GKC_UE ),
-  [TD_S_SS] = ACTION_TAP_DANCE_DOUBLE(KC_S, GKC_SS )
-};
-
-
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
 
@@ -64,9 +49,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
  * ,-----------------------------------------------------------------------------------.
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Z  | U/Ü  |   I  | O/Ö  |   P  | Bksp |
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Z  |   U  |   I  | O    |   P  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Ctrl | A/Ä  | S/ß  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
+ * | Ctrl |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Y  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter/shift |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -74,8 +59,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
-    KC_TAB,  KC_Q,           KC_W,           KC_E,    KC_R,    KC_T,    GKC_Z,   TD(TD_U_UE),    KC_I,    TD(TD_O_OE),    KC_P,     KC_BSPC,
-    KC_LCTL, TD(TD_A_AE),    TD(TD_S_SS),    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,           KC_K,    KC_L,           GKC_SEMI, GKC_QUOT,
+    KC_TAB,  KC_Q,           KC_W,           KC_E,    KC_R,    KC_T,    GKC_Z,   KC_U,           KC_I,    KC_O,          KC_P,     KC_BSPC,
+    KC_LCTL, KC_A,           KC_S,           KC_D,    KC_F,    KC_G,    KC_H,    KC_J,           KC_K,    KC_L,           GKC_SEMI, GKC_QUOT,
     KC_LSFT, GKC_Y,          KC_X,           KC_C,    KC_V,    KC_B,    KC_N,    KC_M,           KC_COMM, KC_DOT,         GKC_SLSH, RSFT_T(KC_ENT),
     KC_ESC,  KC_LEAD,        KC_LALT,        KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,          KC_LEFT, KC_DOWN,        KC_UP,    KC_RGHT
 ),
@@ -122,7 +107,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   {  |   }  |  |   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO ~ |ISO | | Home | End  |      |
+ * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 | Home | End  |      |   ?  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
@@ -130,7 +115,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT_planck_grid(
     GKC_TILD, GKC_EXKL, GKC_AT,  GKC_HASH, GKC_DLR,  GKC_PERC, GKC_CIRC, GKC_AMP,    GKC_ASTR,   GKC_LPARN, GKC_RPARN, KC_BSPC,
     KC_DEL,   KC_F1,    KC_F2,   KC_F3,    KC_F4,    KC_F5,    KC_F6,    GKC_USC,    GKC_PLUS,   GKC_LCUBR, GKC_RCUBR, GKC_PIPE,
-    _______,  KC_F7,    KC_F8,   KC_F9,    KC_F10,   KC_F11,   KC_F12,   S(KC_NUHS), S(KC_NUBS), KC_HOME,   KC_END,    _______,
+    _______,  KC_F7,    KC_F8,   KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_HOME,    KC_END,     _______,   GKC_QUES,  _______,
     _______,  _______,  _______, _______,  _______,  _______,  _______,  _______,    KC_MNXT,    KC_VOLD,   KC_VOLU,   KC_MPLY
 ),
 
@@ -138,7 +123,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  \   |
+ * | Del  |   ß  |   Ä  |   Ö  |   Ü  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  \   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO # |ISO / |Pg Up |Pg Dn |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -147,7 +132,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_RAISE] = LAYOUT_planck_grid(
     GKC_GRAVE,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,     KC_8,    KC_9,      KC_0,      KC_BSPC,
-    KC_DEL,     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   GKC_MINS, GKC_EQ,  GKC_LSQBR, GKC_RSQBR, GKC_BSLS,
+    KC_DEL,     GKC_SS,  GKC_AE,  GKC_OE,  GKC_UE,  KC_F5,   KC_F6,   GKC_MINS, GKC_EQ,  GKC_LSQBR, GKC_RSQBR, GKC_BSLS,
     _______,    KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS,  KC_NUBS, KC_PGUP, KC_PGDN, _______,
     ZELDA,    _______, _______, _______, _______, _______, _______, _______,  KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
--- keyboards/planck/keymaps/vaskosdune/rules.mk
+++ keyboards/planck/keymaps/vaskosdune/rules.mk
@@ -1,3 +1,3 @@
 SRC += muse.c
-TAP_DANCE_ENABLE = yes
+#TAP_DANCE_ENABLE = yes
 LEADER_ENABLE = yes
